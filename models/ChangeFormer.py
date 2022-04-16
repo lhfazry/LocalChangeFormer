f12c3dcacce607af4548b9b6e0899d57d440eca5
Fazry <lhuqita.fazry@gmail.com>
2022-04-17 03:40:57 +0700

    Fixing dataset

--- models/ChangeFormer.py
+++ models/ChangeFormer.py
@@ -1891,14 +1891,14 @@ class LocalChangeFormer(nn.Module):
         super(LocalChangeFormer, self).__init__()
         #Transformer Encoder
         self.embed_dims = [64, 128, 320, 512]
-        self.depths     = [3, 3, 4, 3] #[3, 3, 6, 18, 3]
+        self.depths     = [2, 2, 2, 2] #[3, 3, 6, 18, 3]
         self.embedding_dim = embed_dim
         self.drop_rate = 0.1
         self.attn_drop = 0.1
         self.drop_path_rate = 0.1 
 
         self.Tenc_x2    = EncoderTransformer_v4(img_size=256, patch_size = 7, in_chans=input_nc, num_classes=output_nc, embed_dims=self.embed_dims,
-                 num_heads = [1, 2, 4, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=True, qk_scale=None, drop_rate=self.drop_rate,
+                 num_heads = [1, 1, 1, 1], mlp_ratios=[4, 4, 4, 4], qkv_bias=True, qk_scale=None, drop_rate=self.drop_rate,
                  attn_drop_rate = self.attn_drop, drop_path_rate=self.drop_path_rate, norm_layer=partial(nn.LayerNorm, eps=1e-6),
                  depths=self.depths, sr_ratios=[8, 4, 2, 1])
         
