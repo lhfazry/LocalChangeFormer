d9c7f5cd97daaa9ab8f831c333e58b163fd3293f
Fazry <lhuqita.fazry@gmail.com>
2022-04-17 14:52:58 +0700

    Fixing

--- scripts/run_LocalChangeFormer_LEVIR.sh
+++ scripts/run_LocalChangeFormer_LEVIR.sh
@@ -12,7 +12,7 @@ data_name=LEVIR
 img_size=256    
 batch_size=8   
 lr=0.0001         
-max_epochs=200
+max_epochs=5
 embed_dim=256
 
 net_G=LocalChangeFormer        #ChangeFormerV6 is the finalized verion
